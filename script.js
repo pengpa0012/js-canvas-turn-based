533ffb1f78a004f04d36e78b0df224ea61f5565b
Pengpa0012 <peng.ilikha@gmail.com>
2024-03-24 20:47:32 +0800

    setup canvas, cicle

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 
+
+function drawCircle(x, y) {
+  ctx.beginPath()
+  ctx.arc(x, y, 20, 0, 2 * Math.PI)
+  ctx.fill()
+}
+
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  drawCircle(100, canvas.height - 20)
+}
+
+window.addEventListener("resize", resizeCanvas)
+resizeCanvas()
+
+function animate() {
+  requestAnimationFrame(animate)
+}
+
+// animate()
+
+
+// create full canvas
+// add two ball
+// make ball attack
+// like an arc
+// add health bars
+// turn based combat
+// repeat
+// add 
