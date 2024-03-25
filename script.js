8253d96c7eab2dfe6faa398b61d699dfad64d0c6
Pengpa0012 <peng.ilikha@gmail.com>
2024-03-25 19:45:16 +0800

    added arrow

--- script.js
+++ script.js
@@ -4,13 +4,29 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 let y = canvas.height / 2 - 20
+let arrowAngle = 0
 
 function drawCircle(x, y) {
   ctx.beginPath()
   ctx.arc(x, y, 20, 0, 2 * Math.PI)
   ctx.fill()
+  ctx.stroke()
 }
 
+function drawArrow(x, y, angle) {
+  ctx.save()
+  ctx.translate(x - 20, y)
+  ctx.rotate(angle)
+  ctx.beginPath()
+  ctx.moveTo(0, 0)
+  ctx.lineTo(50, 0)
+  ctx.moveTo(50, 0)
+  ctx.lineTo(40, -10)
+  ctx.moveTo(50, 0)
+  ctx.lineTo(40, 10)
+  ctx.stroke()
+  ctx.restore()
+}
 
 function resizeCanvas() {
   canvas.width = window.innerWidth
@@ -18,22 +34,29 @@ function resizeCanvas() {
   drawCircle(100, canvas.height - 20)
 }
 
+function updateArrowPosition(event) {
+  const rect = canvas.getBoundingClientRect()
+  const mouseX = event.clientX - rect.left
+  const mouseY = event.clientY - rect.top
+  const dx = mouseX - 100
+  const dy = mouseY - (y + 20)
+  arrowAngle = Math.atan2(dy, dx)
+}
+
 window.addEventListener("resize", resizeCanvas)
+window.addEventListener("mousemove", updateArrowPosition)
 resizeCanvas()
 
 function animate() {
   y += 10
-  if(y >= canvas.height - 20) {
+  if (y >= canvas.height - 20) {
     y = canvas.height - 20
   }
-  ctx.clearRect(0,0,canvas.width, canvas.height)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
   drawCircle(100, y)
   drawCircle(canvas.width - 100, y)
+  drawArrow(100 + 20, y, arrowAngle) // Drawing arrow relative to the circle
   requestAnimationFrame(animate)
 }
 
 animate()
-
-// ball can shoot
-// add health bars
-// turn based combat
